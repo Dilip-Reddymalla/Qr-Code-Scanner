4a5ca90fc3255639b5a985bf3fb58ffae9becb8f
Dilip-Reddymalla <reddymaladilip@gmail.com>
2025-06-27 16:43:56 +0530

    Created the files

---
+++ Qr_Scanar.py
@@ -0,0 +1,30 @@
+import cv2
+from pyzbar.pyzbar import decode
+import time
+
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(5, 480)
+
+
+while True:
+    
+    success, frames = cam.read()
+    
+    if not success:
+        print("Error in accessing the camera.")
+        break
+    
+    cv2.imshow("QR Code Scanner", frames)
+
+    scanned_codes = decode(frames)
+    
+    if scanned_codes:
+        for code in scanned_codes:
+            data = code.data.decode('utf-8')
+            print("QR Code Data:", data)
+            time.sleep(5)
+        break
+
+    
---
+++ README.md
@@ -0,0 +1,6 @@
+To run this file in your system
+1. python should be installed other wise instal it.
+2. make sure to give camera permissions to python.
+3. run the file.
+4. show the qr code.
+5. it will show the content in it.
